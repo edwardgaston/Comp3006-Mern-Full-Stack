5bcaf356b3ce1533db6ae11305a5edb8f15764ad
edward-gaston <edward.gaston@students.plymouth.ac.uk>
2024-12-28 16:36:40 +0000

    Created CRUD functions for products, and restructured project files

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
-import Product from './models/product.model.js';
+
+import productRoutes from './routes/product.route.js';
 
 dotenv.config();
 connectDB(); // Ensure the database is connected before handling requests
@@ -9,23 +10,7 @@ connectDB(); // Ensure the database is connected before handling requests
 const app = express();
 app.use(express.json()); // Middleware to parse JSON request bodies
 
-app.post('/api/products', async (req, res) => { // Ensure the route path starts with a '/'
-    const product = req.body; // user will send this data
-
-    if (!product.name || !product.price || !product.image) {
-        return res.status(400).json({ success: false, message: 'Please provide all fields' });
-    }
-
-    const newProduct = new Product(product);
-
-    try {
-        await newProduct.save();
-        res.status(201).json({ success: true, data: newProduct });
-    } catch (error) {
-        console.error("Error in creating product", error.message);
-        res.status(500).json({ success: false, message: error.message });
-    }
-});
+app.use("/api/products", productRoutes);
 
 app.listen(5000, () => {
     console.log('Server started at http://localhost:5000');
