087e179af07097a43bb7edf5a0ec94f3ab4dd30e
edward-gaston <edward.gaston@students.plymouth.ac.uk>
2024-12-28 16:51:24 +0000

    Made improvements to backend

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,11 @@ dotenv.config();
 connectDB(); // Ensure the database is connected before handling requests
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 app.use(express.json()); // Middleware to parse JSON request bodies
 
 app.use("/api/products", productRoutes);
 
-app.listen(5000, () => {
-    console.log('Server started at http://localhost:5000');
+app.listen(PORT, () => {
+    console.log('Server started at http://localhost:' + PORT);
 });
