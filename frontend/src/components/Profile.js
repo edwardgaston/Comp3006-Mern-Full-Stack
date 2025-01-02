ef4ef0e7df167db2cc40906ef87698e9f28f2bc9
edward-gaston <edward.gaston@students.plymouth.ac.uk>
2025-01-02 16:43:27 +0000

    fixed bugs with users

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -2,13 +2,29 @@
 import React, { useContext } from 'react';
 import { Container, Typography, TextField, Button } from '@mui/material';
 import { UserContext } from '../context/UserContext';
+import { useNavigate } from 'react-router-dom';
 
 function Profile() {
   const { user, logout } = useContext(UserContext);
+  const navigate = useNavigate();
 
   const handleLogout = () => {
     logout();
+    navigate('/login');
   };
+
+  if (!user) {
+    return (
+      <Container>
+        <Typography variant="h4" component="h1" gutterBottom>
+          You are not logged in.
+        </Typography>
+        <Button variant="contained" color="primary" onClick={() => navigate('/login')}>
+          Go to Login
+        </Button>
+      </Container>
+    );
+  }
 
   return (
     <Container>
